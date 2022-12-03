b368a737e80317d6fc5a04432822f788b92909ca
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-12-03 15:54:27 +0530

    much padoru stuff done :crossed_swords:

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool playing = true;
+    private UIManager uIManager;
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip gameMusic;
+
+    [SerializeField]
+    private AudioClip endMusic;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,8 +26,23 @@ public class GameManager : MonoBehaviour
         
     }
 
-    void playAgain() 
-    { 
+    public void EndGame() 
+    {
+        playing = false;
+        uIManager.ShowEndScreen();
+        audioSource.Stop();
+        audioSource.loop = false;
+        audioSource.clip = endMusic;
+        audioSource.Play();
+    }
 
+    public bool isPlaying()
+    {
+        return playing;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/Padoru.cs
+++ Assets/Scripts/Padoru.cs
@@ -8,18 +8,29 @@ public class Padoru : MonoBehaviour
     public Rigidbody2D rigidBody;
     public float speed = 6f;
 
+    [SerializeField]
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!gameManager.isPlaying())
+            return;
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             rigidBody.velocity = new Vector2(0, speed);
+        }
+
+        if(transform.position.x < -12f || transform.position.y < -6f)
+        {
+            gameManager.EndGame();
         }
     }
 
